99ddbb24aeb553b9961d6dec1b874423d8dfc579
Ivan G <grikoivan@yandex.ru>
2025-11-28 03:32:44 +0300

    infra added

--- core/main-service/src/main/java/ru/practicum/comment/service/CommentPublicService.java
+++ core/main-service/src/main/java/ru/practicum/comment/service/CommentPublicService.java
@@ -1,4 +1,15 @@
 package ru.practicum.comment.service;
 
-public class CommentPublicService {
+import ru.practicum.comment.dto.CommentDto;
+import ru.practicum.comment.dto.CommentShortDto;
+
+import java.util.List;
+
+public interface CommentPublicService {
+
+    public CommentDto getComment(Long comId);
+
+    public List<CommentShortDto> getCommentsByEvent(Long eventId, int from, int size);
+
+    public CommentDto getCommentByEventAndCommentId(Long eventId, Long commentId);
 }
