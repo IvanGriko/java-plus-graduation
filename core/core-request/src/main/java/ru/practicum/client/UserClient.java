8516e53dafea981918baadf54767d60eeb1a8a3e
Ivan G <grikoivan@yandex.ru>
2025-12-09 02:55:26 +0300

    Add microservices

--- core/core-request/src/main/java/ru/practicum/client/UserClient.java
+++ core/core-request/src/main/java/ru/practicum/client/UserClient.java
@@ -3,6 +3,6 @@ package ru.practicum.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.practicum.api.user.UserApi;
 
-@FeignClient(name = "core-user")
+@FeignClient(name = "user-service")
 public interface UserClient extends UserApi {
 }
