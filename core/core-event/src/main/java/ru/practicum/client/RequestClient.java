9e9313c10503f376f8d0b2ca2e476aa58eeba4f7
Ivan G <grikoivan@yandex.ru>
2025-12-09 03:27:17 +0300

    Add microservices

--- core/core-event/src/main/java/ru/practicum/client/RequestClient.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import ru.practicum.api.request.RequestApi;
-
-@FeignClient(name = "core-request")
-public interface RequestClient extends RequestApi {
-}
--- core/core-request/src/main/java/ru/practicum/client/EventClient.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import ru.practicum.api.event.EventAllApi;
-
-@FeignClient(name = "core-event")
-public interface EventClient extends EventAllApi {
-}
