6d2f165a048c139d9f3be8b554a788c2b19cef53
Ivan G <grikoivan@yandex.ru>
2025-12-12 01:31:11 +0300

    Рабочая версия

--- stats/stats-server/src/test/java/ru/practicum/mockmvc/StatsControllerTest.java
+++ stats/stats-server/src/test/java/ru/practicum/mockmvc/StatsControllerTest.java
@@ -12,9 +12,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.controller.StatsController;
 import ru.practicum.dto.EventHitDto;
 import ru.practicum.dto.EventStatsResponseDto;
-import ru.practicum.controller.StatsController;
 import ru.practicum.service.StatsService;
 
 import java.time.LocalDateTime;
